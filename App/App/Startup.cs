8c90d01ac22b536888744a93a164356c7ed05ece
SunRenat <rynaret@gmail.com>
2020-07-02 19:12:10 +0300

    add API versioning and change c# clients generation

--- App/App/Startup.cs
+++ App/App/Startup.cs
@@ -1,9 +1,13 @@
 using App.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSwag;
+using NSwag.Generation.AspNetCore;
+using System;
 
 namespace App
 {
@@ -19,27 +23,54 @@ namespace App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-
-            // Add OpenAPI/Swagger document
-            services.AddOpenApiDocument(configure =>
-            {
-                configure.SchemaNameGenerator = new CustomSchemaNameGenerator();
-                configure.TypeNameGenerator = new CustomTypeNameGenerator();
-                configure.PostProcess = document =>
+            services
+                .AddApiVersioning(options =>
                 {
-                    document.Info.Version = "v1";
-                    document.Info.Title = "App API";
-                    document.Info.Description = "A simple ASP.NET Core web API";
-                    document.Info.TermsOfService = "None";
-                    document.Info.Contact = new NSwag.OpenApiContact
-                    {
-                        Name = "Renat Sungatullin",
-                        Email = string.Empty,
-                        Url = "some medium"
-                    };
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                })
+                .AddMvc();
+
+            services
+                .AddVersionedApiExplorer(options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+                    options.SubstituteApiVersionInUrl = true;
+                });
+
+            // Add OpenAPI/Swagger documents
+            services
+                .AddOpenApiDocument(configure =>
+                {
+                    BaseConfigure(configure, "v1");
+                })
+                .AddOpenApiDocument(configure =>
+                {
+                    BaseConfigure(configure, "v2");
+                });
+        }
+
+        private void BaseConfigure(AspNetCoreOpenApiDocumentGeneratorSettings configure, string version)
+        {
+            configure.SchemaNameGenerator = new CustomSchemaNameGenerator();
+            configure.TypeNameGenerator = new CustomTypeNameGenerator();
+
+            configure.DocumentName = version;
+            configure.ApiGroupNames = new[] { version };
+
+            configure.PostProcess = document =>
+            {
+                document.Info.Version = version;
+                document.Info.Title = "App API";
+                document.Info.Description = "A simple ASP.NET Core web API";
+                document.Info.TermsOfService = "None";
+                document.Info.Contact = new OpenApiContact
+                {
+                    Name = "Renat Sungatullin",
+                    Email = string.Empty,
+                    Url = "some medium"
                 };
-            });
+            };
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
